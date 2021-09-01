09352629b51c2af263676228fcc10dd188dd944b
polishchuk37a <polishchuk.andrey2002@gmail.com>
2021-09-01 12:32:47 +0300

    made reactive form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MyFormComponent } from './my-form/my-form.component';
 import {HttpClientModule} from "@angular/common/http";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -15,6 +16,8 @@ import {HttpClientModule} from "@angular/common/http";
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/my-form/my-form.component.ts
+++ src/app/my-form/my-form.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {ServiceService} from "../service.service";
-import {pipe} from "rxjs";
 import {tap} from "rxjs/operators";
 import {Data} from "../data";
+import {FormBuilder, FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-my-form',
@@ -11,20 +11,40 @@ import {Data} from "../data";
 })
 export class MyFormComponent implements OnInit {
 
+  form: FormGroup;
   dataJson: Data[] = [];
 
-  constructor(private readonly httpService: ServiceService) { }
+  constructor(private readonly httpService: ServiceService, private formBuilder: FormBuilder) {
+    this.form = this.formBuilder.group({
+      userInputText: new FormControl('')
+    })
+  }
+
+  //searching data with the written word in input from http request
+  // getInfo(writtenWord: string) {
+  //   this.httpService.getData(writtenWord)
+  //     .pipe(
+  //       tap(item => {
+  //         this.dataJson = item.items
+  //       })
+  //     ).subscribe()
+  // }
 
   ngOnInit(): void {
+    this.form.get("userInputText")?.valueChanges.subscribe(selectedValue =>{
+      this.httpService.getData(selectedValue)
+        .pipe(
+          tap(item => {
+            this.dataJson = item.items
+          })
+        ).subscribe()
+
+      console.log(this.form.get("userInputText")?.value)
+    })
   }
 
-  getInfo(writtenWord: string) {
-    this.httpService.getData(writtenWord)
-      .pipe(
-        tap(item => {
-          this.dataJson = item.items
-          console.log(this.dataJson)
-        })
-      ).subscribe()
+  get f(){
+    return this.form.controls;
   }
+
 }
